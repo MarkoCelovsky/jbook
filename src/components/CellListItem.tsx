535791ea0ec0c73b0b80ea3b3299cb31a4d990ae
MarkoCelovsky <marko.celovsky@seakenergetics.com>
2022-10-13 11:14:56 +0200

    feat(s13-s17):somehow i forgot to merge sooner,now that is a lot of sections, section 17 is done

---
+++ src/components/CellListItem.tsx
@@ -0,0 +1,9 @@
+import { ReactElement } from "react";
+
+interface Props {}
+
+const CellListItem = ({}: Props): ReactElement => {
+  return <div>List Item</div>;
+};
+
+export default CellListItem;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,21 @@
-import { createRoot } from "react-dom/client";
-import CodeCell from "./components/CodeCell";
 import "bulmaswatch/superhero/bulmaswatch.min.css";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import TextEditor from "./components/TextEditor";
+import { store } from "./state";
 
 const App = () => {
   return (
     <div>
-      <CodeCell />
+      <TextEditor />
     </div>
   );
 };
 
-const container = document.querySelector("#root");
-const root = createRoot(container!);
-root.render(<App />);
+const container = document.querySelector("#root")!;
+const root = createRoot(container);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
